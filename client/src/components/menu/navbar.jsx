d85e5d4c08309ae77739475822331abe6bc3c0e7
ProjectHelloClub <108552838+BBB-TOAD@users.noreply.github.com>
2024-05-05 16:05:02 -0700

    Make navbar

---
+++ client/src/components/menu/navbar.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "../styles/navbar.css";
+
+function Navbar() {
+  return (
+    <ul id="navbar">
+      <li>
+        <img src="../../src/assets/image.jpeg"></img>
+      </li>
+      <li>
+        <h1>TITLE</h1>
+      </li>
+      <li>
+        <button></button>
+      </li>
+    </ul>
+  );
+}
+
+export default Navbar;
